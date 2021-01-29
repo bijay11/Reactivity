a6cbbf0dd9b1f2d08cc5af7d6fa8444c2e27a6a2
Bijay Tamang <tmg_bijay11@yahoo.com>
2021-01-28 23:27:00 -0500

    Added CRUD funtionality

---
+++ client-app/src/app/models/activity.ts
@@ -0,0 +1,11 @@
+import { StringLiteralLike } from "typescript";
+
+export interface IActivity {
+  id: string;
+  title: string;
+  description: string;
+  category: string;
+  date: string;
+  city: string;
+  venue: string;
+}
